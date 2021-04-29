c7ed2bfd82672da88c46955e8da29dca41141df5
Randall Flagg <russellfannin0@gmail.com>
2021-04-28 17:05:25 -0700

    Exclude raven fury from ring spell pvp slayer negation

--- Source/ACE.Server/WorldObjects/SpellProjectile.cs
+++ Source/ACE.Server/WorldObjects/SpellProjectile.cs
@@ -452,12 +452,13 @@ namespace ACE.Server.WorldObjects
 
             // Possible 2x + damage bonus for the slayer property
             var slayerMod = GetWeaponCreatureSlayerModifier(sourceCreature, target);
-            if (isPVP && slayerMod > 1f)
+            if (isPVP && slayerMod > 1f && Spell.School != MagicSchool.LifeMagic)
             {
                 var spellType = GetProjectileSpellType(Spell.Id);
                 if (spellType == ProjectileSpellType.Ring || spellType == ProjectileSpellType.Wall)
                     slayerMod = 1f;
             }
+
             // life magic projectiles: ie., martyr's hecatomb
             if (Spell.MetaSpellType == ACE.Entity.Enum.SpellType.LifeProjectile)
             {
